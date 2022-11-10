d6a97baad1ec0b133294b8f838514bfb0cf36ac7
Stephen-Ibe <saintkalson@gmail.com>
2022-11-10 07:08:56 +0100

    font and base css setup

--- components/utils/PageHead.tsx
+++ components/utils/PageHead.tsx
@@ -10,6 +10,16 @@ const PageHead = ({ title, description }: Props) => (
     <title>{title}</title>
     <meta name='description' content={description} />
     {/* <link rel='icon' href='/favicon.ico' /> */}
+    <link rel='preconnect' href='https://fonts.googleapis.com' />
+    <link
+      rel='preconnect'
+      href='https://fonts.gstatic.com'
+      crossOrigin='true'
+    />
+    <link
+      href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap'
+      rel='stylesheet'
+    />
   </Head>
 );
 

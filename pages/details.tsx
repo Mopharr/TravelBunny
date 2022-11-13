a5e2a6b9cccb8e28f19523817186d77004d5bd0d
quadri salawudeen <olamiquadri1@gmail.com>
2022-11-13 19:09:40 +0100

    details

--- pages/details.tsx
+++ pages/details.tsx
@@ -1,188 +1,127 @@
 import React from "react";
-
+import Nav from "../components/blocks/NavBar/index";
 import style from "../styles/details.module.css";
-
-// @ts-expect-error this module doesn't have types and I was lazy adding them.
-import Bounce from "react-reveal/Bounce";
+import { BsFillShareFill, BsFillBookmarkFill } from "react-icons/bs";
+import { GrLocation } from "react-icons/gr";
+import { AiFillStar, AiOutlineCheckCircle } from "react-icons/ai";
 
 const Details = () => {
   return (
     <div>
       <div className={style.details}>
+        <Nav />
         <div className={style.b4Div}>
-          <h2 className={style.h22}>
-            Bauchi State flood victims fundraising campaign
-          </h2>
           <div className={style.detailsCap}>
             <div className={style.lef}>
               <img src="/tra2.png" alt="" />
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum
-                viverra tellus, amet proin in semper arcu. Leo quis vitae
-                scelerisque erat urna,i Lorem ipsum dolor sit amet, consectetur
-                adipiscing elit. Rutrum viverra tellus, amet proin in semper
-                arcu. Leo quis vitae scelerisque erat urna,i Lorem ipsum dolor
-                sit amet, consectetur adipiscing elit. Rutrum viverra tellus,
-                amet proin in semper
-              </p>
-              <hr />
+              <div className={style.lText}>
+                <h2>Ikogosi Warm Spring Resort</h2>
+                <div className={style.tIcon}>
+                  <BsFillBookmarkFill className={style.Icon} />
+                  <BsFillShareFill className={style.Icon} />
+                </div>
+              </div>
+              <div className={style.location}>
+                <GrLocation className={style.Icon} />
+                <span>Ekiti State, Nigeria</span>
+              </div>
+              <div className={style.location}>
+                <AiFillStar className={style.star} />
+                <span>4.8</span> |<span className={style.link}>67 Reviews</span>
+              </div>
+              <div className={style.location}>
+                <button>#Lake</button>
+                <button>#swimming</button>
+              </div>
 
-              <span>Created 7 days ago</span>
-              <hr />
+              <div className={style.desc}>
+                <h2>Organizer</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Rutrum viverra tellus, amet proin in semper arcu. Leo quis
+                  vitae scelerisque erat urna,i Lorem ipsum dolor sit amet,
+                  consectetur adipiscing elit. Rutrum viverra tellus, amet proin
+                  in semper arcu. Leo quis vitae scelerisque erat urna,i Lorem
+                  ipsum dolor sit amet, consectetur adipiscing elit. Rutrum
+                  viverra tellus, amet proin in semper
+                </p>
+              </div>
 
               <div className={style.drop}>
                 <h2>Organizer</h2>
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <p className={style.pp}>Solomon Okonkwo</p>
-                    <button>Contact</button>
+                <div className={style.pla}>
+                  <div className={style.pText}>
+                    <AiOutlineCheckCircle className={style.Icon} />{" "}
+                    <span>Warm water swimming pool</span>
+                  </div>
+                  <div className={style.pText}>
+                    <AiOutlineCheckCircle className={style.Icon} />{" "}
+                    <span>Restaurant</span>
+                  </div>
+                  <div className={style.pText}>
+                    <AiOutlineCheckCircle className={style.Icon} />{" "}
+                    <span>Car Park</span>
+                  </div>
+                  <div className={style.pText}>
+                    <AiOutlineCheckCircle className={style.Icon} />{" "}
+                    <span>Warm water swimming pool</span>
+                  </div>
+                  <div className={style.pText}>
+                    <AiOutlineCheckCircle className={style.Icon} />{" "}
+                    <span>Restaurantl</span>
+                  </div>
+                  <div className={style.pText}>
+                    <AiOutlineCheckCircle className={style.Icon} />{" "}
+                    <span>Car Park</span>
                   </div>
                 </div>
               </div>
-
-              <hr />
-
               <div className={style.drop}>
-                <h2>Drop off location</h2>
+                <h2>Review (67)</h2>
 
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <p>Lagos state</p>
-                    <span>
-                      No 21 Olawale Cole Onitiri Avenue, Lekki Phase 1 -
-                      Johathan Irondi +2349033202021{" "}
-                    </span>
+                <div>
+                  <div className={style.orgaN}>
+                    <img src="/tra1.png" alt="" />
+                    <div className={style.name}>
+                      <p>David Adewale</p>
+                      <span>22, October 2022</span>
+                    </div>
                   </div>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur. Eget porttitor neque
+                    pellentesque diam. Felis nunc scelerisque nibh enim nec mi
+                    viverra diam risus. Laoreet lacus dui vel nisl suspendisse
+                    nisi{" "}
+                  </p>
                 </div>
-
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <p>Lagos state</p>
-                    <span>
-                      No 21 Olawale Cole Onitiri Avenue, Lekki Phase 1 -
-                      Johathan Irondi +2349033202021{" "}
-                    </span>
+                <div>
+                  <div className={style.orgaN}>
+                    <img src="/tra1.png" alt="" />
+                    <div className={style.name}>
+                      <p>David Adewale</p>
+                      <span>22, October 2022</span>
+                    </div>
                   </div>
-                </div>
-
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <p>Onitsha, Anambra State</p>
-                    <span>
-                      No 21 Olawale Cole Onitiri Avenue, Lekki Phase 1 -
-                      Johathan Irondi +2349033202021{" "}
-                    </span>
-                  </div>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur. Eget porttitor neque
+                    pellentesque diam. Felis nunc scelerisque nibh enim nec mi
+                    viverra diam risus. Laoreet lacus dui vel nisl suspendisse
+                    nisi{" "}
+                  </p>
                 </div>
               </div>
-              <hr />
-
-              <div className={style.drop}>
-                <h2>Office Account</h2>
-
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <p>Guaranty Trust Bank Naira Account</p>
-                    <span>
-                      Account Number - 0023412632 <br />
-                    </span>
-                    <span>Account Name - Jonathan Irondi</span>
-                  </div>
-                </div>
-
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <p>Access Bank - Dollar Account</p>
-                    <span>
-                      Account Number - 0023412632 <br />
-                    </span>
-                    <span>Account Name - Jonathan Irondi</span>
-                  </div>
-                </div>
-              </div>
-              <hr />
-
-              <div className={style.supp}>
-                <h2>Words Of Support (25)</h2>
-
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <span>Abu Waris</span>
-                    <p>$150,000</p>
-                    <h6>(2 days ago)</h6>
-                  </div>
-                </div>
-
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <span>Abu Waris</span>
-                    <p>$150,000</p>
-                    <h6>(2 days ago)</h6>
-                  </div>
-                </div>
-
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <span>Abu Waris</span>
-                    <p>$150,000</p>
-                    <h6>(2 days ago)</h6>
-                  </div>
-                </div>
-               </div>
             </div>
-            <div className={style.rig}>
-              <div className={style.proG}>
-                <div className={style.proB}></div>
+            <div className={style.rright}>
+              <img src="/tra1.png" alt="" />
+              <div className={style.rig}>
+                <h2>Get access to so much more from our 3rd party partners</h2>
+
+                <button>Find Hotels Around</button>
+                <button>Book a flight to Location</button>
+                <button>Book Taxi</button>
+                <button>Chatter Bus</button>
+                <button>Find Event Around</button>
               </div>
-              <div className={style.nm}>
-                <div className={style.gg}>
-                  <h3>Goal</h3>
-                  <span>$150,000</span>
-                </div>
-                <div className={style.gg}>
-                  <h3>Raise</h3>
-                  <span>$150,000</span>
-                </div>
-              </div>
-              <button className={style.btnD}>Donate Now</button> <br />
-              <button className={style.btnW}>Share</button>
-              <div className={style.dAmt}>
-                <h3>Donation(15)</h3>
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <p>Abu Waris</p>
-                    <span>$150,000</span>
-                    <h6>(2 days ago)</h6>
-                  </div>
-                </div>
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <p>Abu Waris</p>
-                    <span>$150,000</span>
-                    <h6>(2 days ago)</h6>
-                  </div>
-                </div>
-                <div className={style.orgaN}>
-                  <img src="fh1.png" alt="" />
-                  <div className={style.name}>
-                    <p>Abu Waris</p>
-                    <span>$150,000</span>
-                    <h6>(2 days ago)</h6>
-                  </div>
-                </div>
-              </div>
-              <button className={style.btnW}>Share</button>
             </div>
           </div>
         </div>

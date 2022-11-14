647e273ec52c99e1643d734914e9b826bea31f71
Stephen-Ibe <saintkalson@gmail.com>
2022-11-14 09:32:42 +0100

    updates to links

--- components/elements/Modal/index.tsx
+++ components/elements/Modal/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import { BsClipboardPlus, BsPlusCircle } from 'react-icons/bs';
 
 type Props = {
   open: boolean;
@@ -8,13 +9,21 @@ type Props = {
 const Modal = ({ open }: Props) => (
   <div className='modal w-[220px] border z-50 fixed left-[80%] bottom-[8.5rem] rounded-xl bg-[#FF385C] text-white'>
     {open && (
-      <div className='flex flex-col justify-center px-4 py-2 space-y-2'>
-        <Link href='/create' className='py-2'>
-          Create Trip
+      <div className='flex flex-col justify-center space-y-2'>
+        <Link
+          href='/create'
+          className='flex items-center py-4 pl-4 space-x-2 hover:bg-[#ff4d6d] rounded-xl'
+        >
+          <BsPlusCircle size={30} />
+          <span className='text-lg font-semibold'>Create Trip</span>
         </Link>
 
-        <Link href='/attribute' className='py-2'>
-          Post Attraction
+        <Link
+          href='/attribute'
+          className='flex items-center py-4 pl-4 space-x-2 hover:bg-[#ff4d6d] rounded-xl'
+        >
+          <BsClipboardPlus size={30} />
+          <span className='text-lg font-semibold'>Post Attraction</span>
         </Link>
       </div>
     )}

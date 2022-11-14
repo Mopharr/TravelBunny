026958576135c5c9d3a5a3548d5e8ca7f9eb0aa8
Stephen-Ibe <saintkalson@gmail.com>
2022-11-14 06:04:48 +0100

    updates

--- pages/attribute.tsx
+++ pages/attribute.tsx
@@ -1,68 +1,66 @@
-import PageHead from "../components/utils/PageHead";
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import PageHead from '../components/utils/PageHead';
 
-import Nav from "../components/blocks/NavBar/index";
-import style from "../styles/trip.module.css";
+import Nav from '../components/blocks/NavBar/index';
+import style from '../styles/trip.module.css';
 
-const Attribute = () => {
-  return (
-    <div>
-      <PageHead title="Login | Travel Bunny" />
+const Attribute = () => (
+  <div>
+    <PageHead title='Login | Travel Bunny' />
 
-      <div className={style.details}>
-        <Nav />
+    <div className={style.details}>
+      <Nav />
 
-        <div>
-          <h2>Create Trip</h2>
-          <div className={style.bIn}>
-            <div className={style.in}>
-              <label>Trip Title</label>
-              <input type="text" />
-            </div>
-            <div className={style.in}>
-              <label>Location</label>
-              <input type="text" />
-            </div>
+      <div>
+        <h2>Create Trip</h2>
+        <div className={style.bIn}>
+          <div className={style.in}>
+            <label>Trip Title</label>
+            <input type='text' />
           </div>
-          <div className={style.bIn}>
-            <div className={style.in}>
-              <label>Description</label>
-              <textarea name="" id="" cols={30} rows={10}></textarea>
-            </div>
-            <div className={style.in}>
-              <label>Attraction</label>
-              <textarea name="" id="" cols={30} rows={10}></textarea>
-            </div>
+          <div className={style.in}>
+            <label>Location</label>
+            <input type='text' />
           </div>
-          <div className={style.bIn}>
-            <div className={style.in}>
-              <label>Date</label>
-              <input type="text" />
-            </div>
-            <div className={style.in}>
-              <label>Price</label>
-              <input type="text" />
-            </div>
+        </div>
+        <div className={style.bIn}>
+          <div className={style.in}>
+            <label>Description</label>
+            <textarea name='' id='' cols={30} rows={10} />
           </div>
-          <div className={style.bIn}>
-          
-            <div className={style.in}>
-              <label>Hashtags</label>
-              <input type="text" />
-            </div>
-            <div className={style.in}>
-              <label>Add photo</label>
-              <input type="file" />
-            </div>
+          <div className={style.in}>
+            <label>Attraction</label>
+            <textarea name='' id='' cols={30} rows={10} />
           </div>
-          <div className={style.bIn}>
-            <div className={style.in}>
-             <button>Create Animation</button>
-            </div>
+        </div>
+        <div className={style.bIn}>
+          <div className={style.in}>
+            <label>Date</label>
+            <input type='text' />
+          </div>
+          <div className={style.in}>
+            <label>Price</label>
+            <input type='text' />
+          </div>
+        </div>
+        <div className={style.bIn}>
+          <div className={style.in}>
+            <label>Hashtags</label>
+            <input type='text' />
+          </div>
+          <div className={style.in}>
+            <label>Add photo</label>
+            <input type='file' />
+          </div>
+        </div>
+        <div className={style.bIn}>
+          <div className={style.in}>
+            <button type='button'>Create Group Trip</button>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
-export default Attribute
+export default Attribute;
